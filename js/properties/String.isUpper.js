cf515e2d06378ac7ff07c8581dc373fc2dacde53
Mark Moretto <otteromkram@gmail.com>
2020-09-24 12:26:16 -0500

    Updated docstring.
    Added some JSDoc features to documentation string at top of script.

--- js/properties/String.isUpper.js
+++ js/properties/String.isUpper.js
@@ -1,9 +1,11 @@
-/*
-Summary: Create a property for determining whether a String is uppercase or not.
-
+/** 
+ * @author Mark Moretto
+ * @returns {boolean} true if single character is uppercase; false otherwise.
+ * @description String property to evaluate whether a single character is uppercase.
+ * @example <caption>Determine whether character is uppercase.</caption>
+ * // returns true
+ * "A".isUpper
 */
-
-
 
 Object.defineProperty(String.prototype, "isUpper", {
     get() {
@@ -11,12 +13,8 @@ Object.defineProperty(String.prototype, "isUpper", {
     }
 });
 
-/**
- * Assertions
-*/
 
 console.assert("A".isUpper === true, '.isupper(): Single uppercase assertion error.');
 console.assert("a".isUpper === false, '.isupper(): Single lowercase assertion error.');
 console.assert("1".isUpper === true, '.isupper(): Single numeric value assertion error.');
 console.assert("ABC".isUpper === "Property requires single character.", '.isupper(): Multiple character assertion error.');
-
